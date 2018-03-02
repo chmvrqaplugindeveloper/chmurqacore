580d1898b4884ef5ffdf2947ed2cd39eff68c3b7
chmvrqaplugindeveloper <36896699+chmvrqaplugindeveloper@users.noreply.github.com>
2018-03-02 21:01:30 +0100

    added new drops

--- BlockBreakListener.java
+++ BlockBreakListener.java
@@ -24,7 +24,7 @@ public class BlockBreakListener implements Listener {
 				b.getWorld().dropItemNaturally(b.getLocation(), item1);
 				p.sendMessage("§b§lWydropiles diamenta.");
 			}
-			if(Math.random() * 100 <= 3) {
+			if(Math.random() * 100 <= 4) {
 				ItemStack item2 = new ItemBuilder(Material.EMERALD).toItemStack();
 				b.getWorld().dropItemNaturally(b.getLocation(), item2);
 				p.sendMessage("§a§lWydropiles emeralda.");
@@ -33,6 +33,21 @@ public class BlockBreakListener implements Listener {
 				ItemStack item3 = new ItemBuilder(Material.COAL).toItemStack();
 				b.getWorld().dropItemNaturally(b.getLocation(), item3);
 				p.sendMessage("§0§lWydropiles wegiel.");
+			}
+			if(Math.random() * 100 <= 3) {
+				ItemStack item4 = new ItemBuilder(Material.GOLD_INGOT).toItemStack();
+				b.getWorld().dropItemNaturally(b.getLocation(), item4);
+				p.sendMessage("§6lWydropiles zloto.");
+			}
+			if(Math.random() * 100 <= 4) {
+				ItemStack item5 = new ItemBuilder(Material.APPLE).toItemStack();
+				b.getWorld().dropItemNaturally(b.getLocation(), item5);
+				p.sendMessage("§4lWydropiles jablko.");
+			}
+			if(Math.random() * 100 <= 6) {
+				ItemStack item4 = new ItemBuilder(Material.BOOK).toItemStack();
+				b.getWorld().dropItemNaturally(b.getLocation(), item4);
+				p.sendMessage("§2lWydropiles ksiazke.");
 			}
 		}
 	}
